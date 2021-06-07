9517e583807e7f3f732ff6d3c95b4eed367dbc59
Ubuntu <ubuntu@ip-172-31-28-228.ec2.internal>
2021-06-07 11:44:12 +0000

    added directory creation in memory watch service setup script

--- src/6.setup_services.sh
+++ src/6.setup_services.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 set -exu
 # create memory watch service
+mkdir -p ${HOME}/logs/memory
 sudo ln -s ${HOME}/cellxgene_ec2/config/memory_watch.service \
 	/etc/systemd/system/memory_watch.service
 sudo systemctl start memory_watch
